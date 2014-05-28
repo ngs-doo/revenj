d4859615ed0a9c4a0134595494a968f06cf6b915
Rikard Pavelic <rikard@ngs.hr>
2014-05-28 20:41:39 +0200

    Revenj.NET v1.0.0

--- Code/Plugins/NGS.Plugins.DatabasePersistence.Postgres/Properties/AssemblyInfo.cs
+++ Code/Plugins/NGS.Plugins.DatabasePersistence.Postgres/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 
 [assembly: CLSCompliant(true)]
