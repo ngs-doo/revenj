e5542235ca10fb83908e2ad4baa4d29f607d6e78
Rikard Pavelic <rikard@ngs.hr>
2014-11-12 10:16:11 +0100

    LINQ subquery operators improvements.
    Support AVG, MIN and MAX along with SUM operators in subqueries.

--- Code/Database/Revenj.DatabasePersistence.Postgres/QueryGeneration/QueryComposition/SubqueryParts.cs
+++ Code/Database/Revenj.DatabasePersistence.Postgres/QueryGeneration/QueryComposition/SubqueryParts.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using Remotion.Linq.Clauses;
 using Remotion.Linq.Clauses.ResultOperators;
 using Revenj.Common;
 
@@ -61,19 +62,28 @@ namespace Revenj.DatabasePersistence.Postgres.QueryGeneration.QueryComposition
 					var countOperator = ResultOperators.FirstOrDefault(it => it is CountResultOperator || it is LongCountResultOperator);
 					if (countOperator != null)
 						return BuildCountQuery(countOperator);
-					var sumOperator = ResultOperators.FirstOrDefault(it => it is SumResultOperator);
-					if (sumOperator != null)
+					var op =
+						ResultOperators.FirstOrDefault(it => it is SumResultOperator || it is AverageResultOperator
+							|| it is MaxResultOperator || it is MinResultOperator);
+					if (op != null)
 					{
+						var sqlOp = ConvertToSqlOperator(op);
 						if (Selects.Count > 1)
 						{
 							//TOOD hack to fix problem with select
 							Selects.Clear();
 							Selects.Add(new SelectSource { Sql = GetSqlExpression(Selector), ItemType = Selector.Type });
 						}
-						sb.AppendFormat("COALESCE(SUM({0}), 0)", Selects[0].Sql);
+						sb.Append(sqlOp(Selects[0].Sql));
 						//TODO use actual type
 						if (Selects[0].ItemType == typeof(int) || Selects[0].ItemType == typeof(int?))
 							sb.Append("::int");
+						else if (Selects[0].ItemType == typeof(double) || Selects[0].ItemType == typeof(double?))
+							sb.Append("::float");
+						else if (Selects[0].ItemType == typeof(float) || Selects[0].ItemType == typeof(float?))
+							sb.Append("::real");
+						else if (Selects[0].ItemType == typeof(long) || Selects[0].ItemType == typeof(long?))
+							sb.Append("::bigint");
 						if (Selects[0].Name != null)
 							sb.AppendFormat(" AS \"{0}\"", Selects[0].Name);
 						sb.AppendLine();
@@ -103,6 +113,19 @@ namespace Revenj.DatabasePersistence.Postgres.QueryGeneration.QueryComposition
 			ProcessResultOperators(sb);
 
 			return sb.ToString();
+		}
+
+		private static Func<string, string> ConvertToSqlOperator(ResultOperatorBase op)
+		{
+			if (op is AverageResultOperator)
+				return name => "AVG(" + name + ")";
+			if (op is SumResultOperator)
+				return name => "COALESCE(SUM(" + name + "), 0)";
+			if (op is MaxResultOperator)
+				return name => "MAX(" + name + ")";
+			if (op is MinResultOperator)
+				return name => "MIN(" + name + ")";
+			throw new NotSupportedException("Unsupported operator: " + op);
 		}
 	}
 }
