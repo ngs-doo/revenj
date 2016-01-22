5f1f41868c74dbc039577a382d4c563a082d3586
Rikard Pavelic <rikard@ngs.hr>
2016-01-22 12:04:58 +1100

    Java storage fixes.
    Config class should have static methods.

--- java/revenj-storage/src/main/java/org/revenj/Storage.java
+++ java/revenj-storage/src/main/java/org/revenj/Storage.java
@@ -8,8 +8,13 @@ import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 
 public abstract class Storage {
-	public void configure(Container container, Optional<ExecutorService> executorService) {
+	public static void configure(Container container) {
 		Properties properties = container.resolve(Properties.class);
+		Optional<ExecutorService> executorService = container.tryResolve(ExecutorService.class);
+		configure(container, properties, executorService);
+	}
+
+	public static void configure(Container container, Properties properties, Optional<ExecutorService> executorService) {
 		container.registerInstance(S3Repository.class, new AmazonS3Repository(properties, executorService), true);
 	}
 }
