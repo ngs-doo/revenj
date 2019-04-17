477c8c9bf38de445c1b80f3b7ed7e2da4b7775c9
Rikard Pavelic <rikard@ngs.hr>
2019-04-17 13:48:04 +0200

    Signature for tracking dependencies in Scala

--- scala/revenj-core/src/main/scala/net/revenj/patterns/Tracking.scala
+++ scala/revenj-core/src/main/scala/net/revenj/patterns/Tracking.scala
@@ -3,3 +3,7 @@ package net.revenj.patterns
 trait ChangeTracking[T] extends Comparable[T] {
   def getOriginalValue: Option[T]
 }
+
+trait DependencyTracking {
+  def getDependencies: Map[Class[_ <: Identifiable], Seq[Any]]
+}
