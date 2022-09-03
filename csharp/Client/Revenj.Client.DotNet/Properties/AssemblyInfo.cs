ecc4563d1886f5e0a3fc735a3548b41f03e7fa31
Rikard Pavelic <rikard@ngs.hr>
2022-09-03 17:00:20 +0200

    Publish Revenj.Client to Nuget

--- csharp/Client/Revenj.Client.DotNet/Properties/AssemblyInfo.cs
+++ csharp/Client/Revenj.Client.DotNet/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Revenj.Client.DotNet")]
+[assembly: AssemblyTitle("Revenj.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nova Generacija Softvera")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.*")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
