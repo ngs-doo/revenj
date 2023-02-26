174ddea8aa6898d67b54b71cefd338fb727318b7
Rikard Pavelic <rikard@ngs.hr>
2023-02-26 12:29:18 +0100

    Revenj.NET v1.6.3

--- csharp/SharedAssemblyInfo.cs
+++ csharp/SharedAssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("DSL Platform")]
-[assembly: AssemblyCopyright("Copyright © DSL Platform 2022")]
+[assembly: AssemblyCopyright("Copyright © DSL Platform 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
+[assembly: AssemblyVersion("1.6.3.0")]
+[assembly: AssemblyFileVersion("1.6.3.0")]
 [assembly: CLSCompliant(true)]
